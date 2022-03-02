99c0f7a9604832f277ac8ca024b34eb09f3242ad
Marius <marius.stanescu@arnia.ro>
2022-03-02 09:59:21 +0200

    Implement AddOfferRequest model

---
+++ Shared/AddOfferRequest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlazorApp.Shared
+{
+    public class AddOfferRequest
+    {
+        public AddOfferRequest()
+        {
+            Transport = new TransportOfferModel();
+            Housing = new HousingOfferModel();
+        }
+
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public TransportOfferModel Transport { get; set; }
+
+        public HousingOfferModel Housing { get; set; }
+
+        public class TransportOfferModel
+        {
+            public bool IsOffered { get; set; }
+
+            public IEnumerable<string> Cities { get; set; } = new HashSet<string>() { };
+
+            public int AdultCapacity { get; set; }
+
+            public int CarSeatCapacity { get; set; }
+
+            public DateTime? ExpiryDate { get; set; } = DateTime.Today;
+
+            public TimeSpan? ExpiryTime { get; set; } = new TimeSpan(12, 00, 00);
+        }
+
+        public class HousingOfferModel
+        {
+            public bool IsOffered { get; set; }
+
+            public string City { get; set; }
+
+            public int AdultCapacity { get; set; }
+
+            public int ChildrenCapacity { get; set; }
+
+            public bool AllowsPets { get; set; }
+
+            public HousingPeriod Period { get; set; }
+        }
+    }
+}
---
+++ Shared/HousingPeriod.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorApp.Shared
+{
+    public enum HousingPeriod
+    {
+        OneToThreeDays = 10, // 1 - 3 days
+        ThreeDaysToAWeek = 20, // 3 days - 1 week
+        OneToTwoWeeks = 30, // 1 - 2 weeks
+        ThreeToFourWeeks = 40, // 3 - 4 weeks
+        OneToTwoMonths = 50, // 1 - 2 months
+        Indefinite = 60, // indefinite
+    }
+}
