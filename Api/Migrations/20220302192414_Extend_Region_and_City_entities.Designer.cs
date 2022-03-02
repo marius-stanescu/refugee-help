f7dbbcb68a550f08abb94c67a65e599a9ddc07d1
Marius <marius.stanescu@arnia.ro>
2022-03-02 21:27:10 +0200

    Add EF migration to extend Region and City entities

--- Api/Migrations/20220302192414_Extend_Region_and_City_entities.Designer.cs
+++ Api/Migrations/20220302192414_Extend_Region_and_City_entities.Designer.cs
@@ -3,6 +3,7 @@ using BlazorApp.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorApp.Api.Migrations
 {
     [DbContext(typeof(RefugeesDbContext))]
-    partial class RefugeesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220302192414_Extend_Region_and_City_entities")]
+    partial class Extend_Region_and_City_entities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Api/Migrations/20220302192414_Extend_Region_and_City_entities.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp.Api.Migrations
+{
+    public partial class Extend_Region_and_City_entities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayOrder",
+                table: "Region",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ShortName",
+                table: "Region",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DisplayOrder",
+                table: "City",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "City",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "City",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedName",
+                table: "City",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Population",
+                table: "City",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "City",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayOrder",
+                table: "Region");
+
+            migrationBuilder.DropColumn(
+                name: "ShortName",
+                table: "Region");
+
+            migrationBuilder.DropColumn(
+                name: "DisplayOrder",
+                table: "City");
+
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "City");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "City");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedName",
+                table: "City");
+
+            migrationBuilder.DropColumn(
+                name: "Population",
+                table: "City");
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "City");
+        }
+    }
+}
